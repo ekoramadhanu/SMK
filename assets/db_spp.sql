d01eb848abdc720ac5b197b743fa1fd519990a79
Eko Rama <ekorama97@student.ub.ac.id>
2020-03-05 18:24:38 +0700

    db

---
+++ assets/db_spp.sql
@@ -0,0 +1,226 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 05 Mar 2020 pada 12.23
+-- Versi server: 10.1.38-MariaDB
+-- Versi PHP: 7.1.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_spp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `id_kelas` int(11) NOT NULL,
+  `nama_kelas` varchar(10) DEFAULT NULL,
+  `kompetensi_keahlian` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `kelas`
+--
+
+INSERT INTO `kelas` (`id_kelas`, `nama_kelas`, `kompetensi_keahlian`) VALUES
+(1, 'A-11', 'qweasdsf'),
+(3, 'A-12', 'qweqwe');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id_pembayaran` int(11) NOT NULL,
+  `id_petugas` int(11) DEFAULT NULL,
+  `nisn` varchar(10) DEFAULT NULL,
+  `tgl_bayar` date DEFAULT NULL,
+  `bulan_dibayar` varchar(8) DEFAULT NULL,
+  `tahun_dibayar` varchar(4) DEFAULT NULL,
+  `id_spp` int(11) DEFAULT NULL,
+  `jumlah_bayar` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`id_pembayaran`, `id_petugas`, `nisn`, `tgl_bayar`, `bulan_dibayar`, `tahun_dibayar`, `id_spp`, `jumlah_bayar`) VALUES
+(1, 1, '202022', '2020-03-05', '03', '2020', 4, 10000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `petugas`
+--
+
+CREATE TABLE `petugas` (
+  `id_petugas` int(11) NOT NULL,
+  `username` varchar(25) DEFAULT NULL,
+  `password` varchar(32) DEFAULT NULL,
+  `nama_petugas` varchar(35) DEFAULT NULL,
+  `level` enum('admin','petugas') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `petugas`
+--
+
+INSERT INTO `petugas` (`id_petugas`, `username`, `password`, `nama_petugas`, `level`) VALUES
+(1, 'admin', '123456', 'wqe', 'admin'),
+(3, 'eko_ramadhanu', '123456', 'eko', 'petugas');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `nisn` char(10) NOT NULL,
+  `nis` char(8) DEFAULT NULL,
+  `nama` varchar(35) DEFAULT NULL,
+  `alamat` text,
+  `no_telp` varchar(13) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `id_kelas` int(11) DEFAULT NULL,
+  `id_spp` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `siswa`
+--
+
+INSERT INTO `siswa` (`nisn`, `nis`, `nama`, `alamat`, `no_telp`, `password`, `id_kelas`, `id_spp`) VALUES
+('202022', '202032', 'qweqweq', 'qweqweqe', '1231231256', '123456', 3, 4),
+('202024', '202014', 'qweqweq', 'JALAN PANGLIMA SUDIRMAN, NO. 100, BLIMBING, MALANG, 65111', '12312312', '123456', 1, 4),
+('202025', '202015', 'wkwkwkwk', 'JALAN PANGLIMA SUDIRMAN, NO. 100, BLIMBING, MALANG, 65111', '1231231256', '123456', 1, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `spp`
+--
+
+CREATE TABLE `spp` (
+  `id_spp` int(11) NOT NULL,
+  `tahun` int(11) DEFAULT NULL,
+  `nominal` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `spp`
+--
+
+INSERT INTO `spp` (`id_spp`, `tahun`, `nominal`) VALUES
+(3, 2019, 100000),
+(4, 2020, 10000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id_kelas`);
+
+--
+-- Indeks untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id_pembayaran`),
+  ADD KEY `id_petugas` (`id_petugas`),
+  ADD KEY `nisn` (`nisn`),
+  ADD KEY `id_spp` (`id_spp`);
+
+--
+-- Indeks untuk tabel `petugas`
+--
+ALTER TABLE `petugas`
+  ADD PRIMARY KEY (`id_petugas`);
+
+--
+-- Indeks untuk tabel `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`nisn`),
+  ADD KEY `id_kelas` (`id_kelas`),
+  ADD KEY `id_spp` (`id_spp`);
+
+--
+-- Indeks untuk tabel `spp`
+--
+ALTER TABLE `spp`
+  ADD PRIMARY KEY (`id_spp`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `kelas`
+--
+ALTER TABLE `kelas`
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `petugas`
+--
+ALTER TABLE `petugas`
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `spp`
+--
+ALTER TABLE `spp`
+  MODIFY `id_spp` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD CONSTRAINT `pembayaran_ibfk_1` FOREIGN KEY (`id_petugas`) REFERENCES `petugas` (`id_petugas`),
+  ADD CONSTRAINT `pembayaran_ibfk_2` FOREIGN KEY (`nisn`) REFERENCES `siswa` (`nisn`),
+  ADD CONSTRAINT `pembayaran_ibfk_3` FOREIGN KEY (`id_spp`) REFERENCES `siswa` (`id_spp`);
+
+--
+-- Ketidakleluasaan untuk tabel `siswa`
+--
+ALTER TABLE `siswa`
+  ADD CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`),
+  ADD CONSTRAINT `siswa_ibfk_2` FOREIGN KEY (`id_spp`) REFERENCES `spp` (`id_spp`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
